8d682e656643a211fa494b064594b54450dce579
Mesrop Andreasyan <mesropandreasyan@gmail.com>
2018-03-01 21:51:22 +0400

    esim

--- calc.js
+++ calc.js
@@ -93,7 +93,13 @@ const integral=(func,a,b,precision)=>{
   }
   return sum;
 }
-const myBtn = document.getElementById('btn-1');
-    myBtn.addEventListener('click', event => {
-      console.log('1');
-  });
+const textField = document.getElementById("expression-field");
+const buttons = document.querySelectorAll('.button-group button');
+buttons.forEach(currentButton=>{
+  currentButton.addEventListener('click', event => {
+    if(textField.value==='0')
+    textField.value=currentButton.innerText;
+    else
+    textField.value+=currentButton.innerText;
+}
+});
